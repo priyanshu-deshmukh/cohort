7ed9dbd95cb72fb597652809117158e726d432a5
Priyanshu Deshmukh <priyanshu.deshmukh@coditas.com>
2026-06-24 18:17:58 +0530

    refactor/fix Rolechecker. Implement User-Role mapping creation logic

--- app/users/user_service.py
+++ app/users/user_service.py
@@ -4,12 +4,13 @@ from app.users.user_repository import UserRepository as user_repository
 from fastapi import HTTPException, status
 from app.utils.passwords.password_helper import PasswordHelper
 import uuid
-from app.users.user_schema import GetUserResponse
+from app.models.user_role import UserRole
+from app.user_roles.user_role_repository import UserRoleRepository as user_role_repository
 
 class UserService:
 
     @staticmethod
-    def create_new_user(email: str, name: str, plain_password: str, db: Session):
+    def create_new_user(email: str, name: str, plain_password: str, role: str, db: Session):
         existing_user = user_repository.get_user_by_email(email, db)
         
         if existing_user:
@@ -17,12 +18,35 @@ class UserService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="User Already Exists"
             )
-        new_user = User(
+        new_user_record = User(
             email = email,
             name = name,
             hashed_password = PasswordHelper.hash_password(plain_password)
         )
-        return user_repository.create_new_user(new_user, db)
+
+        new_user = user_repository.create_new_user(new_user_record, db)
+        if role == "STUDENT":
+            new_user_role_record = UserRole(
+                user_id = new_user.user_id,
+                role_id = "b2900512-c0bd-47e7-b7ff-57916ef5c9a8"
+            )
+        elif role == "INSTRUCTOR":
+            new_user_role_record = UserRole(
+                user_id = new_user.user_id,
+                role_id = "c14d0343-fbe3-47a8-93f1-2678aad78a7b"
+            )
+        elif role == "COHORT_ADMIN":
+            new_user_role_record = UserRole(
+                user_id = new_user.user_id,
+                role_id = "558620b4-d03d-4332-a882-2154bbfb344d"
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid value for role."
+            )
+        user_role_repository.create_user_role_record(new_user_role_record, db)
+        return new_user
     
 
     @staticmethod
